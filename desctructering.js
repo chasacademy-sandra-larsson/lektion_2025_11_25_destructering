6772b7b1cc250339656cbd008a899ed35eec86ff
chasacademy-sandra-larsson <111293794+chasacademy-sandra-larsson@users.noreply.github.com>
2025-11-25 10:36:06 +0100

    lessons isntructions

---
+++ desctructering.js
@@ -0,0 +1,49 @@
+// ES6 desctructering
+// Gör det enkelt att "packa upp" värden från objekt eller arrayer till variabler
+// Gör koden mer läsbar
+// 
+// Destructering med objekt. Istället för punktnotation!
+// const obj = {
+//         a: 1,
+//         b: 2
+//     }
+//
+// const {a, b } = obj
+//
+// Finns för arrayer 
+// const arr = [1, 2];
+// const [a, b] = arr;
+
+const person = {
+    firstname: "Sandra",
+    age: 29,
+    adress: {
+        street: "Tallvägen 23",
+        city: "Linköping"
+    }
+}
+
+// Punktnotation-sättet
+// const firstname = person.firstname;
+// const age = person.age;
+
+// Objekt destrcutering
+const {firstname, age, adress: {street, city}} = person;
+//const {street, city} = adress;
+console.log(firstname, age, street, city);
+
+// Destructering av array
+
+
+const colors = ["red", "purple", "yellow"];
+
+// Istället för att göra så här:
+const red = colors[0];
+
+
+// Destrukturera - hämta värden från arrayen till vänst om likamedsteckning
+const [col1, col2] = colors;
+console.log(col1, col2);
+
+
+
