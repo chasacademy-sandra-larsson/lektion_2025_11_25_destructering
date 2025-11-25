df4104cef436b866aac1e64a299ef0db4e9fce05
chasacademy-sandra-larsson <111293794+chasacademy-sandra-larsson@users.noreply.github.com>
2025-11-25 13:29:38 +0100

    added chuck norris

---
+++ chuck_norris_api/js/script.js
@@ -0,0 +1,37 @@
+const BASE_URL = "https://api.chucknorris.io/jokes/random";
+const jokeContainer = document.getElementById("joke-container");
+const btnNewJoke = document.getElementById("new-joke-btn");
+
+async function fetchJokes() {
+
+    try {
+         const response = await fetch(`${BASE_URL}`);
+
+        if(!response.ok) {
+            throw new Error("Error " + response.status);
+        }
+        const joke = await response.json();
+
+        console.log(joke)
+
+        renderJoke(joke);
+
+    } catch(error) {
+        console.error("Failed to fetch", error.message);
+    }
+       
+}
+
+function renderJoke(joke) {
+
+    const {icon_url, value} = joke;
+
+    jokeContainer.innerHTML = `
+       <img src="${icon_url}" alt="Chuck Norris Joke">
+       <p>${value}</p>
+    `;
+}
+
+
+
+btnNewJoke.addEventListener("click", fetchJokes);
