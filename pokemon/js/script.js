bb56d0ec2d8914294ab6314366a52c8bb13cc0dc
chasacademy-sandra-larsson <111293794+chasacademy-sandra-larsson@users.noreply.github.com>
2025-11-25 11:42:24 +0100

    lesson ended

--- pokemon/js/script.js
+++ pokemon/js/script.js
@@ -1,6 +1,6 @@
 const pokemonCard = document.getElementById("pokemon-card");
 
-const BASE_URL = "https://pokeapi.co/api/v2/pokemon/10";
+const BASE_URL = "https://pokeapi.co/api/v2/pokemon/5";
 
 async function fetchPokemons() {
     try {
@@ -18,27 +18,44 @@ async function fetchPokemons() {
 
 
 function renderPokemon(data) {
-    console.log(data);
-     console.log(pokemonCard)
+   
     // Render UI
 
+    // Hämtar objekt värden med desttructurering
+
+    const { name,
+            id,
+            sprites: { 
+                other: {
+                    'official-artwork': { front_default: image}
+                }
+            },
+            types: [{type: {name: type}}],
+            stats: [
+                {base_stat: hp},
+                {base_stat: attack},
+                {base_stat: defense}
+            ]
+         } = data;
+
      pokemonCard.innerHTML = `
-        <img src="${data.sprites.other['official-artwork'].front_default}" alt="${data.name}}" class="pokemon-img">
-        <h2 class="pokemon-name">${data.name}</h2>
+        <img src="${image}" alt="${name}}" class="pokemon-img">
+        <h2 class="pokemon-name">${name}</h2>
+        <span class="pokemon-id">#${id}</span>
         <div>
-            <span class="pokemon-type">${data.types[0].type.name}</span>
+            <span class="pokemon-type">${type}</span>
         </div>
         <div class="stats">
             <div class="stat-item">
-                <span class="stat-value">${data.stats[0].base_stat}</span>
+                <span class="stat-value">${hp}</span>
                 <span class="stat-label">HP</span>
             </div>
             <div class="stat-item">
-                <span class="stat-value">${data.stats[1].base_stat}</span>
+                <span class="stat-value">${attack}</span>
                 <span class="stat-label">ATK</span>
             </div>
             <div class="stat-item">
-                <span class="stat-value">${data.stats[2].base_stat}</span>
+                <span class="stat-value">${defense}</span>
                 <span class="stat-label">DEF</span>
             </div>
         </div>
